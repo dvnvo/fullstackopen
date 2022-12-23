4a144a7ec47740b5bdd389f8f0b60410224f8eec
無幻 <111718301+dvnvo@users.noreply.github.com>
2022-12-23 13:49:44 +0800

    using useEffect

--- part2/Phonebook/App.js
+++ part2/Phonebook/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
@@ -14,15 +15,20 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('data---', response.data)
+        setPersons(response.data)
+      })
+  }, [])
 
   const findByName = (name) => {
     if (name === '') {
@@ -31,9 +37,8 @@ const App = () => {
     let len = name.length
     let finded = []
     persons.forEach(element => {
-      // 字符串截取
       let saveName = element.name.toLowerCase().substring(0, len)
-      if (name === saveName) {
+      if (name.toLowerCase() === saveName) {
         console.log("finded", element.name)
         finded.push(element)
       }

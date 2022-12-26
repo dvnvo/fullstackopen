de5df3c73a3710deeaef626c91a1a313ddfafe74
無幻 <111718301+dvnvo@users.noreply.github.com>
2022-12-26 13:52:39 +0800

    3.13 - 3.18

--- part3/mongoPersons.js
+++ part3/mongoPersons.js
@@ -1,47 +1,120 @@
-const mongoose = require('mongoose')
-const dataName = 'personApp'
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
+const app = express()
 
-const password = process.argv[2]
+app.use(cors())
+app.use(express.json())
+// app.use(morgan('combined'))
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
 
-const url =
-  `mongodb+srv://test:${password}@cluster0.fekrece.mongodb.net/${dataName}?retryWrites=true&w=majority`
+app.use(express.static('build'))
 
-//
 
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-const Person = mongoose.model('Person', personSchema)
-
-const nameInput = process.argv[3]
-const numberInput = process.argv[4]
-
-if (nameInput && numberInput) {
-  const person = new Person({
-    name: nameInput,
-    number: numberInput,
-  })
-
-  person.save().then(result => {
-    const msg = `added ${nameInput} number ${numberInput} to phonebook`
-    console.log(msg)
-    mongoose.connection.close()
-  })
-} else {
-  console.log('phonebook: ')
-  Person.find({}).then(result => {
-    result.forEach(person => {
-      console.log(person.name, person.number)
+app.get('/api/persons/info', (request, response) => {
+  Person.count({})
+    .then(sum => {
+      const time = new Date()
+      const msg = `
+        <div>Phonebook has info for ${sum} peoples</div>
+        <div>${time}</div>
+      `
+      response.send(msg)  
     })
-    mongoose.connection.close()
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const p = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, p, { new: true })
+    .then(updatePerson => {
+      response.json(updatePerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  // const id = Number(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+//   const isExist = persons.find(p => p.name === body.name)
+//   if (isExist) {
+//     return response.status(400).json({
+//       error: 'name must be unique'
+//     })
+//   }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
   })
+
+  person.save().then(savePerson => {
+    response.json(savePerson)
+  })
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
 }
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

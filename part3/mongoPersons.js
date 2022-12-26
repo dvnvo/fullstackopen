ea6ddcc8e1a1cb76f5c6a232d46f47b1f6282638
無幻 <111718301+dvnvo@users.noreply.github.com>
2022-12-26 23:52:00 +0800

    3.19-3.20

--- part3/mongoPersons.js
+++ part3/mongoPersons.js
@@ -74,7 +74,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -83,21 +83,16 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-//   const isExist = persons.find(p => p.name === body.name)
-//   if (isExist) {
-//     return response.status(400).json({
-//       error: 'name must be unique'
-//     })
-//   }
-
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then(savePerson => {
-    response.json(savePerson)
-  })
+  person.save()
+    .then(savePerson => {
+      response.json(savePerson)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -105,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
